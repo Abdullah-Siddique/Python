f6b8e0849f7097e82a4925b43e9322b481cd93a0
Abdullah-Siddique <137615484+Abdullah-Siddique@users.noreply.github.com>
2024-08-05 01:01:20 +0600

    classes.py

---
+++ classes.py
@@ -0,0 +1,9 @@
+class student:
+    name = input("Enter your name: ")
+    age = input("Enter your age: ")
+    color = input("Enter your favourite color: ")
+    
+me = student()
+print("Your name is " + me.name)
+print("Your age is " + me.age)
+print("Your favourite color is " + me.color)
